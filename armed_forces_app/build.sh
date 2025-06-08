2c16af61b691fb9ffac84a2aaeb60e9dfd3ee767
Mateo <bantojs@students.national-u.edu.ph>
2025-06-08 22:50:49 +0800

    Move build script to armed_forces_app directory

---
+++ armed_forces_app/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Download Flutter SDK
+curl -L https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.3-stable.tar.xz -o flutter.tar.xz
+
+# Extract Flutter
+tar xf flutter.tar.xz
+
+# Add Flutter to PATH
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Run Flutter doctor
+flutter doctor
+
+# Get dependencies and build
+flutter pub get
+flutter build web 
